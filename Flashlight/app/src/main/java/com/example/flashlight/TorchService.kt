0ec064081f7efc06b8279bbfa1ef88b783a0a5ac
khj94811 <khj94811@naver.com>
2020-04-27 23:25:19 +0900

    Update Torch Service

--- Flashlight/app/src/main/java/com/example/flashlight/TorchService.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/TorchService.kt
@@ -10,14 +10,28 @@ class TorchService : Service() {
         Torch(this)
     }
 
+    private var isRunning = false
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         /* 보통 intent에 action 값을 설정하여 호출 */
         when (intent?.action) {
+            // App에서 실행
             "on" -> {
                 torch.flashOn()
+                isRunning = true
             }
             "off" -> {
                 torch.flashOff()
+                isRunning = false
+            }
+            else -> {
+                isRunning = !isRunning
+                if (isRunning) {
+                    torch.flashOn()
+                }
+                else {
+                    torch.flashOff()
+                }
             }
         }
 
